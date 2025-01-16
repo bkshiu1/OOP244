06cdffc074f384ad772085e2bba8e48b0f41cd2b
bkshiu1 <bkshiu1@myseneca.ca>
2025-01-15 23:17:14 -0500

    Add files via upload

---
+++ OOP244_works/file.h
@@ -0,0 +1,16 @@
+//Carl Shiu, bkshiu@myseneca.ca, 127456192, 15 Jan 2025
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+#pragma once
+#ifndef SENECA_FILE_H_
+#define SENECA_FILE_H_
+namespace seneca {
+	bool openFile(const char filename[]);
+	void closeFile();
+	bool readTitle(char title[]);
+	bool readYear(int* year);
+	bool readMovieRating(char rating[]);
+	bool readDuration(int* duration);
+	bool readGenres(char genres[][11]);
+	bool readConsumerRating(float* rating);
+}
+#endif // !SENECA_FILE_H_
