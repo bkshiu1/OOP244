b5e1e1e6375b2c4e5e28aca6ebbce67d27aae900
bkshiu1 <bkshiu1@myseneca.ca>
2025-01-28 19:07:55 -0500

    Add files via upload

---
+++ CC.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "cstr.h"
+#include "CC.h"
+using namespace std;
+namespace seneca {
+   void CC::prnNumber(unsigned long long no) const {
+      cout << no / 1000000000000ull << " ";
+      no %= 1000000000000ull;
+      cout.fill('0');
+      cout.width(4);
+      cout.setf(ios::right);
+      cout << no / 100000000ull << " ";
+      no %= 100000000ull;
+      cout.width(4);
+      cout << no / 10000ull << " ";
+      no %= 10000ull;
+      cout.width(4);
+      cout << no;
+      cout.unsetf(ios::right);
+      cout.fill(' ');
+   }
+   void CC::display(const char* name, unsigned long long number, short expYear, short expMon, short cvv) const{
+      char lname[31]{};
+      strcpy(lname, name, 30);
+      cout << "| ";
+      cout.width(30);
+      cout.fill(' ');
+      cout.setf(ios::left);
+      cout << lname << " | ";
+      prnNumber(number);
+      cout << " | " << cvv << " | ";
+      cout.unsetf(ios::left);
+      cout.setf(ios::right);
+      cout.width(2);
+      cout << expMon << "/" << expYear << " |" << endl;
+      cout.unsetf(ios::right);
+   }
+   bool CC::validate(const char* name, unsigned long long cardNo, short cvv, short expMon, short expYear) const{
+       if (name == nullptr || sizeof(name) < 2
+           || cardNo < 4000000000000000ull || cardNo > 4099999999999999ull
+           || expMon < 1 || expMon > 12
+           || expYear < 24 || expYear > 32) {
+           return false;
+       }
+       return true;
+   }
+}
--- main.cpp
+++ main.cpp
@@ -1,41 +1,97 @@
-//Karl Shiu, bkshiu1@myseneca.ca, 131531246, 15-1-2025
-//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
 /***********************************************************************
-// OOP244 Workshop #1 : Lab tester program
+// OOP244 Workshop 3 lab: tester program
 //
 // File	main.cpp
 // Version 1.0
-// Date 2024-08-20
-// Author Fardad
+// Date	2024/09/11
+// Author	Fardad Soleimanloo
 // Description
 //
 // Revision History
 // -----------------------------------------------------------
 // Name            Date            Reason
-// Karl Shiu        15-1-2025       
+//
 /////////////////////////////////////////////////////////////////
 ***********************************************************************/
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
-#include "utils.h"
-#include "movie.h"
+#include <cstdio>
+#include "CC.h"
+#include "cstr.h"
 using namespace std;
 using namespace seneca;
-
+void listCCs();
 int main() {
-   bool done = false;
-   char genre[128];
-   loadMovies();
-   cout << "Welcome to 50 top movies of all times \"genre\" search." << endl << endl;
-   while (!done) {
-      cout << "Enter the movie genre to start the search: ";
-      cin >> genre;
-      flushkeys();
-      displayMoviesWithGenre(genre);
-      cout << "Do another search? (Y)es: ";
-      done = !yes();
-      cout << endl;
-   }
-   cout << "Goodbye!" << endl;
+   CC cc;
+   cc.set();
+   cc.set("Hubert Blaine", 4098765423457896, 100);
+   cout << "Valid credit card record: " << endl;
+   cc.display();
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   cout << "Valid credit card with very long name: " << endl;
+   cc.set("Hubert Blaine Wolfeschlegelsteinhausenbergerdorff Sr.", 4098765423457896, 123, 1, 32);
+   cc.display();
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   cout << "Invalid credit card with short name: " << endl;
+   cc.set("H", 4098765423457896, 999, 12, 32);
+   cc.display();
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   cc.set("Hubert Blaine", 4098765423457896, 123, 0, 25);
+   cout << "Invalid credit card record with bad month: " << endl;
+   cc.display();
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   cc.set("Hubert Blaine", 4098765423457896, 123, 13, 25);
+   cout << "Invalid credit card record with bad month: " << endl;
+   cc.display();
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   cc.set("Hubert Blaine", 4098765423457896, 123, 13, 23);
+   cout << "Invalid credit card record with bad year: " << endl;
+   cc.display();
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   cc.set("Hubert Blaine", 4098765423457896, 123, 13, 33);
+   cout << "Invalid credit card record with bad year: " << endl;
+   cc.display();
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   cc.set("Hubert Blaine", 4098765423457896, 99, 13, 33);
+   cout << "Invalid credit card record with bad cvv: " << endl;
+   cc.display();
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   cc.set("Hubert Blaine", 4098765423457896, 1000, 13, 33);
+   cout << "Invalid credit card record with bad cvv: " << endl;
+   cc.display();
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   cc.cleanUp();
+   listCCs();
    return 0;
+}
+void listCCs() {
+   char name[71]{};
+   unsigned long long no{};
+   short ccv{};
+   short expMon{};
+   short expYear{};
+   int numRead{};
+   unsigned int row{};
+   FILE* ccfile = fopen("cc.csv", "r");
+   CC cc;
+   cout << "|     |                                |                     |     | Expiry|" << endl;
+   cout << "| Row | Card holder name               | Credit card number  | CVV | MM/YY |" << endl;
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   cc.set();
+   do {
+      numRead = fscanf(ccfile, "%[^,],%llu,%hd,%hd,%hd\n", name, &no, &ccv, &expMon, &expYear);
+      if (numRead == 5) {
+         cc.set(name, no, ccv, expMon, expYear);
+         cout << "| ";
+         cout.width(3);
+         cout.setf(ios::left);
+         cout << ++row;
+         cout.unsetf(ios::left);
+         cout << " ";
+         cc.display();
+         cc.cleanUp();
+      }
+   } while(numRead==5);
+   cout << "+-----+--------------------------------+---------------------+-----+-------+" << endl;
+   if (ccfile) fclose(ccfile);
 }
