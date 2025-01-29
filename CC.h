403467a30d592a21a5ce6f48522d1a5d7c02af51
bkshiu1 <bkshiu1@myseneca.ca>
2025-01-29 16:48:39 -0500

    Update CC.h

--- CC.h
+++ CC.h
@@ -1,4 +1,4 @@
-//Karl Shiu, 131531246, bkshiu1@myseneca.ca, 29th Jan 2025
+//Carl Shiu, 127456192, bkshiu@myseneca.ca, 29th Jan 2025
 //I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments
 #ifndef SENECA_CC_H_
 #define SENECA_CC_H_
