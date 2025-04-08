ed85610125deeac807bd1bfe43b4f564cea8000a
bkshiu1 <bkshiu1@myseneca.ca>
2025-04-08 12:10:10 -0400

    Update Menu.cpp

--- Menu.cpp
+++ Menu.cpp
@@ -52,27 +52,21 @@ MenuItem::operator bool() const {
 std::ostream& MenuItem::display() const {
     if (m_content && m_content[0] != '\0' && !ut.isspace(m_content)) {
         if (m_rowNumber >= 0) {
-            if (m_indent == 0) {
-                std::cout << " ";
-            }
-            else {
-                std::cout << std::string(m_indent * m_indentSZ, ' ');
-            }
-
+            unsigned indentSpaces = (m_indent == 0) ? 0 : m_indent * m_indentSZ;
+            std::cout << std::string(indentSpaces, ' ');
             if (m_rowNumber < 10) {
                 std::cout << " " << m_rowNumber << "- ";
-            }
-            else {
+            } else {
                 std::cout << m_rowNumber << "- ";
             }
         }
         std::cout << m_content;
-    }
-    else {
+    } else {
         std::cout << "??????????";
     }
     return std::cout;
 }
+
 
 
 
