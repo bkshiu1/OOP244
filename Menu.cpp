03febabba45b40b0c48e233ed54b1c98cdef0fa8
bkshiu1 <bkshiu1@myseneca.ca>
2025-04-14 07:36:17 -0400

    Add files via upload

--- Menu.cpp
+++ Menu.cpp
@@ -67,10 +67,6 @@ std::ostream& MenuItem::display() const {
     return std::cout;
 }
 
-Menu foodMenu("Food Menu", "Back to Order", 2, 3);               // 6 spaces
-Menu foodsizeMenu("Food Size Selection", "Back", 3, 3);              // 9 spaces
-Menu drinkMenu("Drink Menu", "Back to Order", 2, 3);             // 6 spaces
-Menu drinksizeMenu("Drink Size Selection", "Back", 3, 3);             // 9 spaces
 
 
 Menu::Menu(const char* title, const char* exitOption, unsigned indent, unsigned indentSZ)
