1571685ba1370f3f8180a9c7b4d99d01e93c6e60
bkshiu1 <bkshiu1@myseneca.ca>
2025-04-14 05:49:31 -0400

    Add files via upload

--- Drink.cpp
+++ Drink.cpp
@@ -11,71 +11,98 @@
 // Date      Reason
 // 2025/04/08  Implemented Drink module logic
 // 2025/04/13  Implemented ms3 requirements
-// 2025/04/14  Updated for ms4: added clone()
+// 2025/04/14  Updated for ms4: added clone(), fixed display
 // -----------------------------------------------------------
 // I have done all the coding by myself and only copied the code
 // that my professor provided to complete my workshops and assignments.
 // -----------------------------------------------------------
 ***********************************************************************/
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <iomanip>
-#include <string>
+#include <fstream>
 #include <cstring>
+#include <string>
 #include "Drink.h"
 #include "Menu.h"
+#include "Utils.h"
 
 using namespace std;
 
 namespace seneca {
 
-    Drink::Drink() : m_ordered(false), m_size('\0') {}
-    Drink::~Drink() {}
-    Drink::Drink(const Drink& src)
-        : Billable(src), m_ordered(src.m_ordered), m_size(src.m_size) {
-    }
+    Drink::Drink() : m_size(0), m_ordered(false) {}
+
+    Drink::Drink(const Drink& src) : Billable(src), m_size(src.m_size), m_ordered(src.m_ordered) {}
+
     Drink& Drink::operator=(const Drink& src) {
         if (this != &src) {
             Billable::operator=(src);
-            m_ordered = src.m_ordered;
             m_size = src.m_size;
+            m_ordered = src.m_ordered;
         }
         return *this;
     }
 
-    ostream& Drink::print(ostream& ostr) const {
-        ostr << left << setw(28) << setfill('.') << (const char*)*this;
+    Drink::~Drink() {}
 
-        switch (m_size) {
-        case 'S': ostr << "SML.."; break;
-        case 'M': ostr << "MID.."; break;
-        case 'L': ostr << "LRG.."; break;
-        case 'X': ostr << "XLR.."; break;
-        default:  ostr << "????."; break;
+    std::ostream& Drink::print(std::ostream& os) const {
+        os << left << setw(28) << setfill('.') << (const char*)*this;
+
+        if (!m_ordered) {
+            os << ".....";
+        }
+        else {
+            switch (m_size) {
+            case 'S':
+                os << "SML..";
+                break;
+            case 'M':
+                os << "MED..";
+                break;
+            case 'L':
+                os << "LRG..";
+                break;
+            case 'X':
+                os << "XLRG.";
+                break;
+            default:
+                os << ".....";
+            }
         }
 
-        ostr << right << setw(7) << setfill(' ') << fixed << setprecision(2) << price();
-        return ostr;
+        os << right << setw(7) << setfill(' ') << fixed << setprecision(2) << price();
+        return os;
     }
 
     bool Drink::order() {
-        Menu sizeMenu("Drink Size Selection", "Back", 1, 5);
+        Menu sizeMenu("Drink Size Selection", "Back", 5, 1);
         sizeMenu << "Small" << "Medium" << "Large" << "Extra Large";
-        int sel = sizeMenu.select();
+        int selection = sizeMenu.select();
 
-        const char codes[] = { 'S', 'M', 'L', 'X' };
-        if (sel >= 1 && sel <= 4) {
-            m_size = codes[sel - 1];
-            m_ordered = true;
-        }
-        else {
-            m_ordered = false;
+        switch (selection) {
+        case 1:
+            m_size = 'S';
+            break;
+        case 2:
+            m_size = 'M';
+            break;
+        case 3:
+            m_size = 'L';
+            break;
+        case 4:
+            m_size = 'X';
+            break;
+        default:
+            m_size = 0;
         }
 
+        m_ordered = (m_size != 0);
         return m_ordered;
     }
 
     bool Drink::ordered() const {
-        return m_size == 'S' || m_size == 'M' || m_size == 'L' || m_size == 'X';
+        return m_ordered;
     }
 
     std::ifstream& Drink::read(std::ifstream& file) {
@@ -84,25 +111,32 @@ namespace seneca {
 
         if (getline(file, name, ',') && file >> price) {
             file.ignore(1000, '\n');
-            setName(name.c_str());  // <-- Make sure this sets the Billable name
+            setName(name.c_str());
             setPrice(price);
-            m_size = '\0';
+            m_ordered = false;
+            m_size = 0;
         }
-
         return file;
     }
 
     double Drink::price() const {
-        double multiplier = 0.0;
-        switch (m_size) {
-        case 'S': multiplier = 0.5; break;
-        case 'M': multiplier = 0.75; break;
-        case 'L': multiplier = 1.0; break;
-        case 'X': multiplier = 1.5; break;
-        }
-        return getBasePrice() * multiplier;
-    }
+        if (!m_ordered) return 0.0;
 
+        double base = getBasePrice();
+
+        switch (m_size) {
+        case 'S':
+            return base * 0.75;
+        case 'M':
+            return base * 1.00;
+        case 'L':
+            return base * 1.50;
+        case 'X':
+            return base * 2.00;
+        default:
+            return 0.0;
+        }
+    }
 
     Billable* Drink::clone() const {
         return new Drink(*this);
