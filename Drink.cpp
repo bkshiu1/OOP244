5d1b004096bc481a4183a99542ffac7c5a027678
bkshiu1 <bkshiu1@myseneca.ca>
2025-04-13 22:11:45 -0400

    Add files via upload

--- Drink.cpp
+++ Drink.cpp
@@ -10,6 +10,7 @@
 // -----------------------------------------------------------
 // Date      Reason
 // 2025/04/08  Implemented Drink module logic
+// 2025/04/13  Fixed special instruction <ENTER> input handling, incorrect indentation
 // 2025/04/13  Implemented ms3 requirements
 // -----------------------------------------------------------
 // I have done all the coding by myself and only copied the code
